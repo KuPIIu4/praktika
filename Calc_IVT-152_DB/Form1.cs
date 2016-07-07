0f1ffde1ddad71950d6b379b5907ca4404a6506a
OpakyJl <opakyjll@gmail.com>
2016-07-07 12:48:09 +0600

    update xml comments

--- Calc_IVT-152_DB/Form1.cs
+++ Calc_IVT-152_DB/Form1.cs
@@ -66,5 +66,10 @@ namespace Calc_IVT_152_DB
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
     }
 }
--- Calc_IVT-152_DB/TwoArgsCalculator/ExponentCalculator.cs
+++ Calc_IVT-152_DB/TwoArgsCalculator/ExponentCalculator.cs
@@ -5,11 +5,17 @@ namespace Calc_IVT_152_DB.TwoArgsCalculator
     public class ExponentCalculator : ITwoArgumentsCalculator
     {
         /// <summary>
-        /// 
+        /// raises a number to power
         /// </summary>
-        /// <param name="firstArgument"></param>
-        /// <param name="secondArgument"></param>
-        /// <returns></returns>
+        /// <param name="firstArgument">
+        /// number
+        /// </param>
+        /// <param name="secondArgument">
+        /// power
+        /// </param>
+        /// <returns>
+        /// result of powering
+        /// </returns>
         public double Calculate(double firstArgument, double secondArgument)
         {
             if (firstArgument < 0.0 && secondArgument < 1 && secondArgument != 0)
